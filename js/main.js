e355c023221f50120003b4dae3a9e68deefbc105
Feerr S <feerrsilvav@gmail.com>
2021-11-06 02:08:51 -0600

    Creamos nuestro form en html, para mejorar la Ux

--- js/main.js
+++ js/main.js
@@ -1,12 +1,28 @@
-const precioOriginal = 100;
-const descuento = 15;
+// const precioOriginal = 100;
+// const descuento = 15;
 
-const porcentajePrecioDescuento = 100 - descuento;
-const precioDescuento = (precioOriginal * porcentajePrecioDescuento) / 100;
 
-console.log({
-    precioOriginal,
-    porcentajePrecioDescuento,
-    precioDescuento,
+const calcularPrecioConDescuento = (precio, descuento) => {
+    const porcentajePrecioDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioDescuento) / 100;
 
-});
+    return precioConDescuento;
+}
+
+// Escribiendo html desde javascript
+
+const doItCalc = () => {
+    const inputPrice = document.getElementById("input-price");
+    const inputDiscount = document.getElementById('input-discount');
+
+    const priceValue = inputPrice.value;
+    const discountValue = inputDiscount.value;
+
+    const precioFinal = calcularPrecioConDescuento(priceValue,discountValue);
+
+    const resultPrice = document.getElementById('result');
+    resultPrice.innerHTML = `Tu precio con descuento es de: $${precioFinal}`
+
+}
+
+
